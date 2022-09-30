44d0c0e5dd8e90fdf836106d47e61f4fc52af7d8
backFox <jorealbusiness@gmail.com>
2022-10-01 01:00:15 +0900

    ## 서적과 버전을 동일화

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import App from "./App";
 import Login from "./Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import SignUp from "./SignUp";
@@ -23,11 +23,17 @@ class AppRouter extends React.Component {
       <div>
         <Router>
           <div>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/" element={<App />} />
-            </Routes>
+            <Switch>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/signup">
+                <SignUp />
+              </Route>
+              <Route path="/">
+                <App />
+              </Route>
+            </Switch>
           </div>
           <Box mt={5}>
             <Copyright />
